1d5240b2b441ef94ec5a977daebd1602d08e21ae
Gabriel Cardoso <gabrielcardosodeveloper@gmail.com>
2022-08-16 01:50:58 -0300

    style(Create transactions container to envolve content off page): :lipstick:

--- src/pages/Transactions/styles.ts
+++ src/pages/Transactions/styles.ts
@@ -1,3 +1,9 @@
 import styled from 'styled-components'
 
-export const TransactionsContainer = styled.main``
+export const TransactionsContainer = styled.main`
+  width: 100%;
+  max-width: 1120px;
+
+  margin: 4rem auto 0;
+  padding: 0 1.5rem;
+`
