f86cdf55d475e1adec190b0a366a1e4dc43071b4
Gabriel Cardoso <gabrielcardosodeveloper@gmail.com>
2022-08-16 01:07:24 -0300

    feat(Add header and summary components to transactions page): :sparkles:

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,12 +1,14 @@
 import React from 'react'
 
 import { Header } from '../../components/Header'
+import { Summary } from '../../components/Summary'
 import { TransactionsContainer } from './styles'
 
 export function Transactions() {
   return (
     <TransactionsContainer>
       <Header />
+      <Summary />
     </TransactionsContainer>
   )
 }
