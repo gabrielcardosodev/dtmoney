fb2482ab89db577bfb6d8e26fb235a8501c3330c
Gabriel Cardoso <gabrielcardosodeveloper@gmail.com>
2022-08-30 11:59:25 -0300

    feat(Add new transaction modal to header): :sparkles:

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,6 +1,8 @@
+import * as Dialog from '@radix-ui/react-dialog'
 import React from 'react'
 
 import logoImg from '../../assets/logo.svg'
+import { NewTransactionModal } from '../NewTransactionModal'
 import { NewTransactionButton, HeaderContainer, HeaderContent } from './styles'
 
 export function Header() {
@@ -9,7 +11,13 @@ export function Header() {
       <HeaderContent>
         <img src={logoImg} alt="" />
 
-        <NewTransactionButton>Nova transação</NewTransactionButton>
+        <Dialog.Root>
+          <Dialog.Trigger asChild>
+            <NewTransactionButton>Nova transação</NewTransactionButton>
+          </Dialog.Trigger>
+
+          <NewTransactionModal />
+        </Dialog.Root>
       </HeaderContent>
     </HeaderContainer>
   )
