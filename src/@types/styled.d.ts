3edf576701490a643bf90078a20c568fac576dd2
Gabriel Cardoso <gabrielcardosodeveloper@gmail.com>
2022-08-31 08:24:40 -0300

    fix(Fix theme interface bug): :bug:

--- src/@types/styled.d.ts
+++ src/@types/styled.d.ts
@@ -1,8 +1,9 @@
+/* eslint-disable @typescript-eslint/no-empty-interface */
 import 'styled-components'
 import { defaultTheme } from '../styles/themes/default'
 
 type ThemeType = typeof defaultTheme
 
 declare module 'styled-components' {
-  export type DefaultTheme = ThemeType
+  export interface DefaultTheme extends ThemeType {}
 }
