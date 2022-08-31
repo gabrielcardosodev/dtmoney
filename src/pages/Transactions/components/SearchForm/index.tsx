a235ff59146a6a9ebc48029e829ece8e0ddcf87b
Gabriel Cardoso <gabrielcardosodeveloper@gmail.com>
2022-08-31 08:26:10 -0300

    feat(Create search form schema and function to submit form): :sparkles:

--- src/pages/Transactions/components/SearchForm/index.tsx
+++ src/pages/Transactions/components/SearchForm/index.tsx
@@ -1,5 +1,8 @@
+import { zodResolver } from '@hookform/resolvers/zod'
 import { MagnifyingGlass } from 'phosphor-react'
 import React from 'react'
+import { useForm } from 'react-hook-form'
+import * as z from 'zod'
 
 import {
   SearchFormContainer,
@@ -7,11 +10,35 @@ import {
   SearchFormSubmitButton,
 } from './styles'
 
+const searchFormSchema = z.object({
+  query: z.string().trim(),
+})
+
+type SearchFormInputs = z.infer<typeof searchFormSchema>
+
 export function SearchForm() {
+  const {
+    register,
+    handleSubmit,
+    formState: { isSubmitting },
+  } = useForm<SearchFormInputs>({
+    resolver: zodResolver(searchFormSchema),
+  })
+
+  async function handleSearchTransactions(data: SearchFormInputs) {
+    await new Promise((resolve) => setTimeout(resolve, 2000))
+
+    console.log(data)
+  }
+
   return (
-    <SearchFormContainer>
-      <SearchFormInput placeholder="Busque uma transação" />
-      <SearchFormSubmitButton type="submit">
+    <SearchFormContainer onSubmit={handleSubmit(handleSearchTransactions)}>
+      <SearchFormInput
+        placeholder="Busque uma transação"
+        {...register('query')}
+      />
+
+      <SearchFormSubmitButton type="submit" disabled={isSubmitting}>
         <MagnifyingGlass size={20} weight="bold" />
         Buscar
       </SearchFormSubmitButton>
