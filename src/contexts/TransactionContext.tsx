2e8e551f89d06caf485312cd28c9fcae9c1954b7
Gabriel Cardoso <gabrielcardosodeveloper@gmail.com>
2022-09-02 10:00:42 -0300

    feat(Create a function with responsability to create a new transaction): :sparkles:

--- src/contexts/TransactionContext.tsx
+++ src/contexts/TransactionContext.tsx
@@ -3,9 +3,17 @@ import React, { createContext, useEffect, useState } from 'react'
 import { Transaction } from '../@types'
 import { api } from '../lib/axios'
 
+interface CreateTransactionInputType {
+  description: string
+  price: number
+  category: string
+  type: 'income' | 'outcome'
+}
+
 interface TransactionsContextType {
   transactions: Transaction[]
   fetchTransaction: (query?: string) => Promise<void>
+  createTransaction: (data: CreateTransactionInputType) => Promise<void>
 }
 
 interface TransactionProviderProps {
@@ -20,6 +28,8 @@ export function TransactionsProvider({ children }: TransactionProviderProps) {
   async function fetchTransaction(query?: string) {
     const response = await api.get('transactions', {
       params: {
+        _sort: 'createdAt',
+        _order: 'desc',
         q: query,
       },
     })
@@ -27,12 +37,28 @@ export function TransactionsProvider({ children }: TransactionProviderProps) {
     setTransactions(response.data)
   }
 
+  async function createTransaction(data: CreateTransactionInputType) {
+    const { description, type, category, price } = data
+
+    const response = await api.post('transactions', {
+      description,
+      type,
+      category,
+      price,
+      createdAt: new Date(),
+    })
+
+    setTransactions((state) => [response.data, ...state])
+  }
+
   useEffect(() => {
     fetchTransaction()
   }, [])
 
   return (
-    <TransactionsContext.Provider value={{ transactions, fetchTransaction }}>
+    <TransactionsContext.Provider
+      value={{ transactions, fetchTransaction, createTransaction }}
+    >
       {children}
     </TransactionsContext.Provider>
   )
