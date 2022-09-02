cdf2304df00c2586f0fed234da152e90485ef821
Gabriel Cardoso <gabrielcardosodeveloper@gmail.com>
2022-09-02 11:32:58 -0300

    feat(Involve summary calc in useMemo): :sparkles:

--- src/hooks/useSummary.ts
+++ src/hooks/useSummary.ts
@@ -1,3 +1,4 @@
+import { useMemo } from 'react'
 import { useContextSelector } from 'use-context-selector'
 
 import { TransactionsContext } from '../contexts/TransactionContext'
@@ -7,25 +8,27 @@ export function useSummary() {
     return context.transactions
   })
 
-  const summary = transactions.reduce(
-    (acc, transaction) => {
-      if (transaction.type === 'income') {
-        acc.income += transaction.price
-        acc.total += transaction.price
-      } else {
-        acc.outcome += transaction.price
-        acc.total -= transaction.price
-      }
+  const summary = useMemo(() => {
+    transactions.reduce(
+      (acc, transaction) => {
+        if (transaction.type === 'income') {
+          acc.income += transaction.price
+          acc.total += transaction.price
+        } else {
+          acc.outcome += transaction.price
+          acc.total -= transaction.price
+        }
 
-      return acc
-    },
+        return acc
+      },
 
-    {
-      income: 0,
-      outcome: 0,
-      total: 0,
-    },
-  )
+      {
+        income: 0,
+        outcome: 0,
+        total: 0,
+      },
+    )
+  }, [transactions])
 
   return summary
 }
